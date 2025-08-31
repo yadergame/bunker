8cbb1a7acaca80a9d2b044a11eae6aff6cec740d
yadergame <114643236+yadergame@users.noreply.github.com>
2025-08-31 21:12:41 +0300

    Add info page and Create Item

---
+++ src/Models/Game/Item.php
@@ -0,0 +1,25 @@
+<?php
+namespace src\Models\Game;
+
+use Exception;
+
+class Item
+{
+    public int $ID;
+    public string $Name;
+    public int $Valeu;
+    public function __construct(int $id,string $name,int $value)
+    {
+        $this->ID = $id;
+        $this->Name = $name;
+        $this->Valeu = $value;
+    }
+    static public function Sum(Item $item1,Item $item2):Item
+    {
+        if($item1->ID == $item2->ID)
+        return new Item($item1->ID,$item1->Name,$item1->Valeu+$item2->Valeu);
+        else throw new Exception("Сложение разных вещей невозможно");
+    }
+}
+
+?>
